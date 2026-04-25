b1b66f87ed3bf9b399587b5305198c94aa35b1f7
HappySummer <zjhao233@126.com>
2026-04-25 23:45:07 +0800

    refactor(DiscoveryView): 移除未使用的scrollContainerRef引用

--- src/components/DiscoveryView.tsx
+++ src/components/DiscoveryView.tsx
@@ -885,7 +885,6 @@ export const DiscoveryView: React.FC = React.memo(() => {
       />
 
       <div
-        ref={scrollContainerRef}
         className="flex flex-col gap-4 lg:flex-row lg:gap-6 flex-1 min-h-0 min-w-0 items-start"
       >
         <div
